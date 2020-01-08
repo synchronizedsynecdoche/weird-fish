01581b824ab01f8df470cc1653656f4caba74762
patrick <kelly237@purdue.edu>
2020-01-08 18:25:38 -0500

    added checks on IDs, added documentation, switched from format to f strings

--- weird_fish/Timelines.py
+++ weird_fish/Timelines.py
@@ -1,4 +1,6 @@
-import requests, json
+import json
+
+import requests
 
 class Timelines:
     
@@ -6,19 +8,53 @@ class Timelines:
 
         self.endpoint = url + "/api/v1/timelines/"
     
-    def get_public(self, local=False, only_media=0, max_id=-1,
-                   since_id=None, min_id=None, limit=20):
+    def get_public(self, local=False, only_media=False, max_id=0,
+                   since_id=0, min_id=0, limit=20):
+    
+        """Gets the latest public posts from the timeline (what you see on the homepage)
+
+        Args:
+            local: Do we want to get posts from other nodes our instance is federated with?
+            
+            only_media: Do we want to only see posts containing media?
+
+            max_id: Maximum (API) ID to fetch, note that the ID seen on an instance's website is not necessarily the ID used by the API
+
+            since_id: Earliest API ID to fetch.
+
+            min_id: Minimum API ID to fetch.
+
+            limit: How many posts do we want? Vanilla Mastodon defaults to 20.
+
+        Returns: 
+            A Python dictionary from the JSON returned by /api/v1/timelines/public
+
+        Raises:
+
+            A ValueError when passed IDs that won't be understood by Mastodon.
+
+        """
+        
+        if since_id < min_id or min_id > max_id or min_id < 0:
+            raise ValueError("min_id must be less than since_id and max_id and all must be at least 0")
+
+        
 
         #build the request
-        to_send = self.endpoint + "public" + "?local={}&only_media={}&max_id={}&since_id={}&min_id={}&limit={}".format(local, only_media, max_id, since_id, min_id, limit)
-        print(to_send)
+        to_send = self.endpoint + f"public?local={local}&only_media={only_media}&limit={limit}"
+        
+        if max_id:
+            to_send += f"&max_id={max_id}".format(max_id)
+
+        if min_id:
+            to_send += f"&min_id={min_id}"
+
+        if since_id:
+            to_send += f"&since_id={since_id}"
 
         #send!
         response = requests.get(to_send)
+
         unraveled = json.loads(response.text)
         
         return unraveled
-
-my_tl = Timelines("https://mastodon.social")
-
-print(my_tl.get_public())
